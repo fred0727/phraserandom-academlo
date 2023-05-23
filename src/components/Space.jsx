f66d11372419f7736a24dcfb213fa05b3977e740
fred0727 <freddymh07952023@gmail.com>
2023-05-23 11:09:12 -0500

    updating images

--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -3,10 +3,10 @@ import "./styles/Space.css";
 
 const Space = ({ bgImg }) => {
   const images = {
-    back1: "./public/images/planeta1.svg",
-    back2: "./public/images/planeta2.svg",
-    back3: "./public/images/planeta3.svg",
-    back4: "./public/images/planeta4.svg",
+    back1: "./images/planeta1.svg",
+    back2: "./images/planeta2.svg",
+    back3: "./images/planeta3.svg",
+    back4: "./images/planeta4.svg",
   };
   return (
     <div className="space--world">
